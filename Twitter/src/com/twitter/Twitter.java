ef22897320550aeaf522665a38217bce035c0241
Lex996 <rakicaleksandra1@gmail.com>
2017-04-11 00:03:51 +0200

    Napisana JavaDoc dokumentaciju za klasu Twitter, njene atribute i metode

--- Twitter/src/com/twitter/Twitter.java
+++ Twitter/src/com/twitter/Twitter.java
@@ -3,12 +3,35 @@ package com.twitter;
 
 	import java.util.LinkedList;
 	import com.twitter.poruke.TwitterPoruka;
+	
+	
+	/**
+	 * Klasa Twitter ima atribut poruke i metode vratiSvePoruke, unesi i vratiPoruke
+	 * @version 1.0
+	 * @author Aleksandra Rakic
+	 *
+	 */
 	public class Twitter {
+		
+	/**
+	 * Atribut poruke koji predstavlja LinkedList<TwitterPoruka>
+	 */
 	private LinkedList<TwitterPoruka> poruke =
 	new LinkedList<TwitterPoruka>();
+	
+	/**
+	 * Metoda koja vraca listu svih poruka
+	 * @return  LinkedList<TwitterPoruka>
+	 */
 	public LinkedList<TwitterPoruka> vratiSvePoruke(){
 	return poruke;
 	}
+	
+	/**
+	 * Metoda koja kreira novi objekat TwitterPoruka i postavlja vrednosti njegovih atributa(korisnik, poruka). Pourka se unosi u listu poruka.
+	 * @param korisnik String koji predstavlja vrednost atributa korisnik koja se postavlja
+	 * @param poruka String koji predstavlja vrednost atributa poruka koja se postavlja
+	 */
 	public void unesi(String korisnik, String poruka) {
 	//Pravi se nova poruka i puni podacima.
 	TwitterPoruka tp = new TwitterPoruka();
@@ -17,6 +40,13 @@ package com.twitter;
 	//Poruka se unosi u listu na kraj
 	poruke.addLast(tp);
 	}
+	
+	/**
+	 * Metoda koja kreira i vraca niz poruka koje sadrze trazeni tag
+	 * @param maxBroj int koji oznacava maksimalan broj poruka u nizu 
+	 * @param tag String predstavlja trazeni tag u poruci
+	 * @return TwitterPoruka[] niz poruka koje sadrze trazeni tag
+	 */
 	public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) {
 	if (tag==null || tag.isEmpty())
 	throw new RuntimeException("Morate uneti tag");
