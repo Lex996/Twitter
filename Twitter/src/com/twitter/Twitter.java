f4c42e7e5d71a2695551c88ee4ed24efde9e7ef7
Lex996 <rakicaleksandra1@gmail.com>
2017-04-11 12:51:44 +0200

    Ispravljena greska u meotdi vratiPoruke, svi testovi prolaze.

--- Twitter/src/com/twitter/Twitter.java
+++ Twitter/src/com/twitter/Twitter.java
@@ -65,7 +65,7 @@ package com.twitter;
 	for (int i = 0; i < poruke.size(); i++)
 	if (poruke.get(i).getPoruka().indexOf(tag)!=-1)
 	if (brojac < maxBroj){
-	rezultat[brojac+1]=poruke.get(i);
+	rezultat[brojac]=poruke.get(i);
 	brojac++;
 	}
 	else break;
